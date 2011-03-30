c5a06e1066cb6db67f3eb14ab829692ad4f99f1a
Anant Narayanan <anant@kix.in>
2011-03-29 17:35:16 -0700

    Forgot to add changes from last commit

--- js/functions.js
+++ js/functions.js
@@ -23,6 +23,10 @@ function onStateChange(type, arg) {
     }
 }
 
+$(window).unload(function() {
+    Me.endSession();
+});
+
 $(document).ready(function($) {
     
     if (!window.navigator.service ||
@@ -33,17 +37,25 @@ $(document).ready(function($) {
         return;
     }
     
-    // Start preview 
     Me = window.navigator.service.media;
     App = window.navigator.apps;
     Ctx = document.getElementById('tehcanvas').getContext("2d");
+    
+    // Preload Image
+    var img = new Image();
+    img.src = "i/rainbow.png";
+    img.onload = function() {
+        Ctx.drawImage(img, 272, 172);
+    }
+    
+    // Start preview 
     Me.beginSession(
         {'width':800,'height':600,'audio':false}, Ctx, onStateChange
     );
     
     // Take a picture
     $('#ohSnap').bind('click', function() {
-        
+        var canvas  = $('#canvas');
         var numbers = $('#numbers');
         var dots    = $('#dots');
         var flash   = $('#flash');
@@ -68,17 +80,26 @@ $(document).ready(function($) {
         setTimeout(function(){
             ohSnap.remove();
         }, 1000);    
-            
+        setTimeout(function(){
+            canvas.addClass('fadeOut');
+        }, 2000);
+        
         // Display a camera flash after the countdown (3 second delay)
         setTimeout(function(){
-            $('tehcanvas').hide();
             flash.show(0, function() {
                 $(this).addClass('fadeOut');  
             });
             numbers.remove();
             
             var data = Me.fetchImage();
-            sendImage(data);
+            Me.endSession();
+            
+            canvas.show(0, function() {
+                canvas.addClass('fadeIn');
+            });
+            $('#flickr').bind('click', function() {
+                sendImage(data);
+            });
         }, 3000);
         
         // Fade in the redo and flickr buttons, give focus to the textarea
@@ -89,9 +110,6 @@ $(document).ready(function($) {
             $('#compose').show(0, function() {
                 $(this).addClass('fadeIn');
             });
-            $('#tehcanvas').show(0, function() {
-                $(this).addClass('fadeIn');
-            });
             $('#area').focus();
         }, 4000);
     });
@@ -99,5 +117,5 @@ $(document).ready(function($) {
     // Insert default twitter message 
     // TO DO: limit to 140 characters
     $('#area').val('I just took a photograph using Rainbow by Mozilla Labs!');
-
+    $('#redo').bind('click', function() window.location.reload());
 });
